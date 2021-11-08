202807835811e560c5546427e2192c403711b3a2
Théo DE ALMEIDA <56266959+theo-dealmeida@users.noreply.github.com>
2021-11-08 11:12:30 +0100

    Delete MesAllumettes/app directory

--- MesAllumettes/app/src/main/java/com/example/mesallumettes/joueur/InteractionHumaine.java
+++
@@ -1,8 +0,0 @@
-package com.example.mesallumettes.joueur;
-
-public interface InteractionHumaine {
-
-    public int obtenirNbChoisi(); // pour voir combien d’allumettes ont été choisies
-    public Object getSynchro(); // pour fournir un objet de synchronisation, en vue d’un wait
-
-}
